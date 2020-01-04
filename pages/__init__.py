0323bfc42671cc73c5758a59b1edcf127175e553
Steve Cassidy <steve.cassidy@mq.edu.au>
2020-01-04 12:51:02 +1100

    Bump version: 0.1.0 → 0.2.0

--- pages/__init__.py
+++ pages/__init__.py
@@ -4,6 +4,6 @@ Pages with hierarchical structure.
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 default_app_config = 'pages.apps.PagesConfig'  # pylint: disable=invalid-name
